7117649005d5601456b981c4041394ab2eb25ac9
Xuenew <18188108851@163.com>
2024-04-18 19:58:38 +0800

    add:添加了忽略文件

--- bot/self_zidingyi/zidingyi_unit_bot.py
+++ bot/self_zidingyi/zidingyi_unit_bot.py
@@ -10,7 +10,7 @@ from config_xyy import WX_URL  # 微信机器人去请求拿到链接数据
 from config import available_setting
 
 def get_shortvideo(search_key):
-    url = "{}/rtb_search/search/an_interesting_short_play".format(WX_URL)  # 公网
+    url = "{}/rtb_search/search/an_interesting_short_play/".format(WX_URL)  # 公网
     # url = "http://192.168.31.95:5000/rtb_search/search/an_interesting_short_play/"
 
     payload = {'keyword': search_key}
